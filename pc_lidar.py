d5dc3bd49bce67201bbc8ba198225f59518d84c5
Rafael Teodosio <rafzteodosio@gmail.com>
2021-04-25 21:43:08 -0300

    add process point algo

---
+++ pc_lidar.py
@@ -0,0 +1,30 @@
+import numpy as np
+import laspy as lp
+
+input_path = "./"
+output_path = "./"
+
+dataname = "NZ19_Wellington"
+point_cloud = lp.file.File(input_path+dataname+".las", mode="r")
+
+points = np.vstack((point_cloud.x, point_cloud.y, point_cloud.z)).transpose()
+colors = np.vstack((point_cloud.red, point_cloud.green,
+                   point_cloud.blue)).transpose()
+
+voxel_size=6
+nb_vox=np.ceil((np.max(points, axis=0) - np.min(points, axis=0))/voxel_size)
+
+non_empty_voxel_keys, inverse, nb_pts_per_voxel= np.unique(((points - np.min(points, axis=0)) // voxel_size).astype(int), axis=0, return_inverse=True, return_counts=True)
+idx_pts_vox_sorted=np.argsort(inverse)
+voxel_grid={}
+grid_barycenter,grid_candidate_center=[],[]
+last_seen=0
+
+for idx,vox in enumerate(non_empty_voxel_keys):
+  voxel_grid[tuple(vox)]=points[idx_pts_vox_sorted[last_seen:last_seen+nb_pts_per_voxel[idx]]]
+  grid_barycenter.append(np.mean(voxel_grid[tuple(vox)],axis=0))
+  grid_candidate_center.append(voxel_grid[tuple(vox)][np.linalg.norm(voxel_grid[tuple(vox)]-np.mean(voxel_grid[tuple(vox)],axis=0),axis=1).argmin()])
+  last_seen+=nb_pts_per_voxel[idx]
+  
+np.savetxt(output_path+dataname+"_voxel-best_point_%s.xyz" % (voxel_size), grid_candidate_center, delimiter=" ", fmt="%1.15f")
+
---
+++ pointcloud_to_3d.py
@@ -0,0 +1,17 @@
+import numpy as np
+import open3d as o3d
+
+input_path = "./"
+output_path = "./"
+dataname = "NZ19_Wellington_voxel-best_point_6.xyz"
+point_cloud = np.loadtxt(input_path+dataname, skiprows=1)
+
+# The following command first instantiates the Open3d point cloud object
+# then add points, color and normals to it from the original NumPy array.
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(point_cloud[:, :3])
+#pcd.colors = o3d.utility.Vector3dVector(point_cloud[:, 3:6]/255)
+#pcd.normals = o3d.utility.Vector3dVector(point_cloud[:,6:9])
+o3d.visualization.draw_geometries([pcd])
+
